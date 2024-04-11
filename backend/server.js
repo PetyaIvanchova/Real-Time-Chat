a93beb18e03dec7e84760012c17fa6edc39cd09d
Petya Ivanchova <pepivanchova@yahoo.com>
2024-04-11 16:19:18 +0300

    add auth setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
@@ -7,7 +9,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req,res) => {
-    res.send('Hell0o world');
+    res.send('Hello world');
 })
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
