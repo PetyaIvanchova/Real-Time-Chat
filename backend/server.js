b052752dedf639e8b34c94eeb628883fa5a01357
Petya Ivanchova <pepivanchova@yahoo.com>
2024-04-11 22:00:38 +0300

    signup with jwt and cookie

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,10 @@ import authRoutes from './routes/auth.routes.js';
 import connectToDB from './db/connectToDB.js';
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res) => {
-    res.send('Hello world');
-})
-
+dotenv.config();
+app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () =>{
