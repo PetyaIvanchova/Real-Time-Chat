b436052a206fef6a31c7635c54b9b845030d91e7
Petya Ivanchova <pepivanchova@yahoo.com>
2024-04-12 12:38:02 +0300

    add sendMessage endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToDB from './db/connectToDB.js';
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.listen(PORT, () =>{
     connectToDB();
