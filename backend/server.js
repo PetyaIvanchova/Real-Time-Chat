f1d15c2c4b99ae765b413576cd4ba38be0e0fe52
Petya Ivanchova <pepivanchova@yahoo.com>
2024-04-15 13:11:08 +0300

    add socket io with shake and sound notification for the messages

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectToDB from './db/connectToDB.js';
 
-const app = express();
+import {app, server} from './socket/socket.js';
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -18,7 +19,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-app.listen(PORT, () =>{
+server.listen(PORT, () =>{
     connectToDB();
     console.log(`Server is running on port ${PORT}`);
 })
